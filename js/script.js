f61e5418d801ae697854aca6065a4b4072d6af58
Veronika <verona.binko@gmail.com>
2021-08-26 10:42:28 +0300

    feat: add functions hideTabContent and showTabContent

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
+window.addEventListener('DOMContentLoaded', () => {
 
+  const tabs = document.querySelectorAll('.tabheader__item');
+  const tabsContent = document.querySelectorAll('.tabcontent');
+  const tabsParent = document.querySelector('.tabheader__items');
+
+  function hideTabContent() {
+    tabsContent.forEach(item => {
+      item.style.display = 'none';
+    });
+    
+    tabs.forEach(item => {
+      item.classList.remove('tabheader__item_active');
+    });
+  }
+
+  function showTabContent(i) {
+    tabsContent[i].style.display = 'block';
+    tabs[i].classList.add('tabheader__item_active');
+  }
+
+  hideTabContent();
+  showTabContent(0);
+});
 
