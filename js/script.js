18ac1194fd8ef2a37898a313680f3778d44f76d5
Veronika <verona.binko@gmail.com>
2021-09-01 00:32:10 +0300

    add modal in .js

--- js/script.js
+++ js/script.js
@@ -91,4 +91,50 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   setClock('.timer', deadline);
+
+  // Modal
+
+  const modalTrigger = document.querySelectorAll('[data-modal]');
+  const modalCloseBtn = document.querySelector('[data-close]');
+  const modal = document.querySelector('.modal');
+
+  // function showModal(modal) {
+  //   modal.classList.remove('none');
+  //   modal.classList.add('block');
+  // }
+
+  // modalTrigger.forEach(item => {
+  //   item.addEventListener('click', () => {
+  //     modal.classList.remove('none');
+  //     modal.classList.add('block');
+  //   });
+  // })
+
+  modalTrigger.forEach(item => {
+    item.addEventListener('click', () => {
+      modal.classList.add('show');
+      modal.classList.remove('hide');
+      document.body.style.overflow = 'hidden';
+    })
+  })
+
+  function closeModal() {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    document.body.style.overflow = '';
+  }
+
+  modalCloseBtn.addEventListener('click', closeModal)
+  
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  })
+
+  document.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape' && modal.classList.contains('show')) {
+      closeModal();
+    }
+  })
 });
